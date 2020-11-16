0e27fc5815edfc4d46f74781ebaa8fea737d109f
vova verh <spamv@i.ua>
2020-11-16 13:46:48 +0200

    change index-3

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -53,14 +53,11 @@ function write_argument()
 function argument_1()
 {
     git checkout tester
-    cp $home_dir/hello-world/examlpe-index/index-1.jsp $home_dir/hello-world/webapp/src/main/webapp/index.jsp
+    cp $home_dir/examlpe-index/index-1.jsp $home_dir/webapp/src/main/webapp/index.jsp
     git add -A
     git commit -m "change index-1"
-    #git push --all
-    #git merge master
     git checkout master
     git merge tester 
-    #git merge -m "merge tester changes 1" tester --no-ff
     git add -A
     git commit -m "merge tester changes 1"
     git push origin
@@ -73,7 +70,7 @@ function argument_1()
 function argument_2()
 {
     git checkout tester
-    cp $home_dir/hello-world/examlpe-index/index-2.jsp $home_dir/hello-world/webapp/src/main/webapp/index.jsp
+    cp $home_dir/examlpe-index/index-2.jsp $home_dir/webapp/src/main/webapp/index.jsp
     git add -A
     git commit -m "change index-2"
     git checkout master
@@ -90,7 +87,7 @@ function argument_2()
 function argument_3()
 {
     git checkout tester
-    cp $home_dir/hello-world/examlpe-index/index-3.jsp $home_dir/hello-world/webapp/src/main/webapp/index.jsp
+    cp $home_dir/examlpe-index/index-3.jsp $home_dir/webapp/src/main/webapp/index.jsp
     git add -A
     git commit -m "change index-3"
     git checkout master
@@ -107,7 +104,7 @@ function argument_3()
 function argument_e()
 {
     git checkout tester
-    cp $home_dir/hello-world/examlpe-index/index-error.jsp $home_dir/hello-world/webapp/src/main/webapp/index.jsp
+    cp $home_dir/examlpe-index/index-error.jsp $home_dir/webapp/src/main/webapp/index.jsp
     git add -A
     git commit -m "change index-e"
     git checkout master
